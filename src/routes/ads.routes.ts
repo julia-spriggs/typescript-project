93448c60027b0abf939f86b8b9f1be99e4f60ac6
Julia Spriggs <wildcode@julia-spriggs.fr>
2023-11-15 11:52:56 +0100

    created routes and types

---
+++ src/routes/ads.routes.ts
@@ -0,0 +1,90 @@
+import { Router, Request, Response } from "express";
+import { Ad } from "../types/ads";
+import { ads } from "../data";
+import AdServices from "../services/ads.services";
+const router = Router();
+
+router.post("/create", function (req: Request, res: Response) {
+  const {
+    id,
+    description,
+    location,
+    createdAt,
+    owner,
+    picture,
+    price,
+    title,
+  }: Ad = req.body;
+  const isAlreadyInData: boolean = ads.some((ad) => ad.id === id);
+  if (isAlreadyInData) {
+    throw new Error("Cette annonce existe déjà");
+  }
+
+  const result: Ad[] = new AdServices().create({
+    id,
+    description,
+    location,
+    createdAt,
+    owner,
+    picture,
+    price,
+    title,
+  })
+  // ads.push({
+  //   id,
+  //   description,
+  //   location,
+  //   createdAt,
+  //   owner,
+  //   picture,
+  //   price,
+  //   title,
+  // });
+  res.send(result);
+});
+
+router.get("/list", function (req: Request, res: Response) {
+  res.send(ads);
+});
+router.get("/find/:id", function (req: Request, res: Response) {
+  const id = +req.params.id;
+  const ad = ads.find((item) => item.id === id);
+  if (!ad) {
+    return res
+      .status(404)
+      .send({ message: "L'annonce n'existe pas", success: false });
+  }
+  res.send(ad);
+});
+
+router.patch("/update/:id", function (req: Request, res: Response) {
+  const id = +req.params.id;
+  const data: Partial<Ad> = req.body;
+  //! prévoir que on envoi pas tout le data, mais que on envoi que les clés qui ont été renseignées
+  if (!data) {
+    return res
+      .status(400)
+      .send({ message: "Vérifiez vos informations", success: false });
+  }
+  const updateIndex = ads.findIndex((item) => item.id === id);
+  if (updateIndex === -1) {
+    return res
+      .status(404)
+      .send({ message: "L'annonce n'existe pas", success: false });
+  }
+  ads[updateIndex] = { ...ads[updateIndex], ...data };
+  res.send(ads[updateIndex]);
+});
+router.delete("/delete/:id", function (req: Request, res: Response) {
+  const id = +req.params.id;
+  const index = ads.findIndex((ad) => ad.id === id);
+  if (index === -1) {
+    return res
+      .status(404)
+      .send({ message: "L'annonce n'existe pas", success: false });
+  }
+  ads.splice(index, 1);
+  res.send(ads);
+});
+
+export default router;
